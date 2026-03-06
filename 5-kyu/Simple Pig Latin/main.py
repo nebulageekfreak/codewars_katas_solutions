2268183998d8d20abd6c4860840acacf3d22bc88
nebula <nebulageek@protonmail.com>
2026-03-06 06:35:54 +0500

    Update main.py

--- 5-kyu/Simple Pig Latin/main.py
+++ 5-kyu/Simple Pig Latin/main.py
@@ -1,1 +1,6 @@
-Simple Pig Latin
+def pig_it(text):
+    lst = text.split()
+    for i, v in enumerate(lst):
+        if v not in '!?':
+            lst[i] = v[1:] + v[0] + 'ay'
+    return ' '.join(lst)
